84ad8f1f5b06b619954a4fea79ca82f44481713c
vlad <vlad.markuts@gmail.com>
2020-04-26 01:39:43 +0300

    'page for each blog post (SHOW route)'

--- app.js
+++ app.js
@@ -70,6 +70,17 @@ app.post('/blogs', function(req,res){
   });
 });
 
+//SHOW route
+app.get('/blogs/:id', function(req,res){
+  Blog.findById(req.params.id, function(err, foundBlog){
+    if (err) {
+      res.redirect('/blogs');
+    } else {
+      res.render('show', {blog: foundBlog});
+    }
+  });
+});
+
 
 
 app.listen(port, function() {
